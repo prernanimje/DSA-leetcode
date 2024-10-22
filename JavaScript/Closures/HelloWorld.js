0d56cd7be58da59d49311c18d6e2ad27acecf55c
prernanimje <prernanimje5@gmail.com>
2024-10-22 17:27:59 +0530

    2667. Create Hello World Function

---
+++ JavaScript/Closures/HelloWorld.js
@@ -0,0 +1,16 @@
+// 2667. Create Hello World Function
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+
+//Leetcode Method Code
+
+// var createHelloWorld = function() {
+    
+//     return function(...args) {
+//         return "Hello World"
+//     }
+// };
+
+//simple code
+console.log("Hello World")
+
