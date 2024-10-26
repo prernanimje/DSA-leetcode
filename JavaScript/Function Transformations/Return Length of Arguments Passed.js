4da622beeba98ef4b6b5118c63883930881fe034
prernanimje <prernanimje5@gmail.com>
2024-10-26 23:37:33 +0530

    2703. Return Length of Arguments Passed

---
+++ JavaScript/Function Transformations/Return Length of Arguments Passed.js
@@ -0,0 +1,6 @@
+//2703. Return Length of Arguments Passed
+//Write a function argumentsLength that returns the count of arguments passed to it.
+
+var argumentsLength = function(...args) {
+    return args.length
+};
