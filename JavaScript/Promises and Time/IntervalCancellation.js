e05c18f51c172faaca31aef6881b26555af7b6a8
prernanimje <prernanimje5@gmail.com>
2024-10-30 20:07:38 +0530

    2725. Interval Cancellation

---
+++ JavaScript/Promises and Time/IntervalCancellation.js
@@ -0,0 +1,15 @@
+//2725. Interval Cancellation
+// Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+// After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+// setTimeout(cancelFn, cancelTimeMs)
+// The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
+
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+
+    let cancelFn = () => clearInterval(timer);
+    return cancelFn;
+};
